9fbb80530a3d5105b8764dd90a4fed539a22879d
krig6 <ej.ldsm@gmail.com>
2026-06-19 07:37:22 +0800

    feat(Navbar): add navbar component
    - Add Home, Shop, and Cart navigation links
    - Scaffold basic navigation structure before styling and routing

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+export const Navbar = () => (
+    <nav>
+        <ul>
+            <li>
+                <a href="home">Home</a>
+            </li>
+            <li>
+                <a href="shop">Shop</a>
+            </li>
+            <li>
+                <a href="cart">Cart</a>
+            </li>
+        </ul>
+    </nav>
+)
