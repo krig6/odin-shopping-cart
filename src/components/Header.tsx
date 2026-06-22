3f8bcdd3d73431ba7604ff53891e83a26d74cc6b
krig6 <ej.ldsm@gmail.com>
2026-06-22 22:13:29 +0800

    feat(Header): create header component
    - Compose Navbar, SearchBox, and HeaderActions inside a semantic header
    element
    - Establish a top-level layout component for shared page navigation and
    actions

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import { Navbar } from './Navbar'
+import { SearchBox } from './SearchBox'
+import { HeaderActions } from './HeaderActions'
+
+export const Header = () => (
+    <header>
+        <Navbar />
+        <SearchBox />
+        <HeaderActions />
+    </header>
+)
