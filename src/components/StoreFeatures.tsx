815903ead4c39b7ff87c73f8eab55cd9e8c6e61b
krig6 <ej.ldsm@gmail.com>
2026-07-09 18:21:20 +0800

    feat(StoreFeatures): create store features section
    - Create StoreFeatures component
    - Display store service highlights

---
+++ src/components/StoreFeatures.tsx
@@ -0,0 +1,23 @@
+export const StoreFeatures = () => (
+    <section className="store-features">
+        <div className="store-feature">
+            <h4>Instant Delivery</h4>
+            <p>Get your games instantly</p>
+        </div>
+
+        <div className="store-feature">
+            <h4>Secure Payment</h4>
+            <p>100% secure checkout</p>
+        </div>
+
+        <div className="store-feature">
+            <h4>Great Prices</h4>
+            <p>Best deals always</p>
+        </div>
+
+        <div className="store-feature">
+            <h4>24/7 Support </h4>
+            <p>We're here to help</p>
+        </div>
+    </section>
+)
