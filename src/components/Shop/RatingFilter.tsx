8a193f32c7f22effc096579228c7ae0bd2c4eb05
krig6 <ej.ldsm@gmail.com>
2026-07-17 20:19:13 +0800

    feat(Shop): render rating filter as checkboxes
    - Replace static rating display with checkbox inputs
    - Associate labels with checkbox inputs
    - Keep star icons alongside rating labels

--- src/components/Shop/RatingFilter.tsx
+++ src/components/Shop/RatingFilter.tsx
@@ -5,18 +5,18 @@ const RATINGS = [5, 4, 3, 2, 1]
 export const RatingFilter = () => (
     <div>
         {RATINGS.map((rating) => (
-            <div>
-                <div key={rating}>
-                    {Array.from({ length: 5 }, (_, index) =>
-                        index < rating ? (
-                            <Star key={index} fill="#FFC000" />
-                        ) : (
-                            <Star key={index} fill="#808080" />
-                        )
-                    )}
-                </div>
+            <label key={rating} htmlFor={`rating-${rating}`}>
+                <input id={`rating-${rating}`} type="checkbox" value={rating} />
+
+                {Array.from({ length: 5 }, (_, index) =>
+                    index < rating ? (
+                        <Star key={index} fill="#FFC000" />
+                    ) : (
+                        <Star key={index} fill="#808080" />
+                    )
+                )}
                 <span>& Up</span>
-            </div>
+            </label>
         ))}
     </div>
 )
