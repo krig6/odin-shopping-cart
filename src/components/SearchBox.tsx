ac12bb205c0524f0631df1fb745669bacb0510f4
krig6 <ej.ldsm@gmail.com>
2026-07-28 19:19:09 +0800

    feat(SearchBox): style search input

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,5 +1,9 @@
 export const SearchBox = () => (
     <div>
-        <input type="text" placeholder="Search..." />
+        <input
+            className="rounded-full bg-[#334155] px-4 py-1 text-slate-100"
+            type="text"
+            placeholder="Search..."
+        />
     </div>
 )
