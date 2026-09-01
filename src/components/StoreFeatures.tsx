7358436d9a28ba22290517cde57724ff0d20b19d
krig6 <ej.ldsm@gmail.com>
2026-09-01 21:27:12 +0800

    feat(StoreFeatures): style store features
    - Add feature icons for each store benefit
    - Arrange icons and feature details with flexbox
    - Add spacing, background, padding, and text styling

--- src/components/StoreFeatures.tsx
+++ src/components/StoreFeatures.tsx
@@ -1,28 +1,40 @@
+import { HeadphoneMic, PriceTag, Shield, Truck } from '@boxicons/react'
+
 const STORE_FEATURES = [
     {
+        icon: <Truck />,
         title: 'Instant Delivery',
         description: 'Get your games instantly',
     },
     {
+        icon: <Shield />,
         title: 'Secure Payment',
         description: '100% secure checkout',
     },
     {
+        icon: <PriceTag />,
         title: 'Great Prices',
         description: 'Best deals always',
     },
     {
+        icon: <HeadphoneMic />,
         title: '24/7 Support',
         description: "We're here to help",
     },
 ]
 
 export const StoreFeatures = () => (
-    <section className="store-features">
+    <section className="flex justify-between rounded-md bg-gray-500 p-5 text-white">
         {STORE_FEATURES.map((feature) => (
-            <article key={feature.title}>
-                <h4>{feature.title}</h4>
-                <p>{feature.description}</p>
+            <article
+                key={feature.title}
+                className="flex flex-row items-center justify-center gap-3 text-center"
+            >
+                {feature.icon}
+                <div>
+                    <h4 className="font-semibold">{feature.title}</h4>
+                    <p className="text-xs">{feature.description}</p>
+                </div>
             </article>
         ))}
     </section>
