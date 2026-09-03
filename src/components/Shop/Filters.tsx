2943f2b62cfdc7de9310dc454922e181218524b6
krig6 <ej.ldsm@gmail.com>
2026-09-03 20:56:29 +0800

    refactor(Shop): compose filters in Filters component
    - Pass filter state and handlers through Filters
    - Move genre and rating filter composition into Filters
    - Update Shop to render the new Filters component

--- src/components/Shop/Filters.tsx
+++ src/components/Shop/Filters.tsx
@@ -1,13 +1,35 @@
 import { GenreFilter } from './GenreFilter'
 import { RatingFilter } from './RatingFilter'
 
-export const Filters = () => (
-    <aside>
-        <header>
-            <h3>Filters</h3>
-            <button type="button">Clear All</button>
-        </header>
-        <GenreFilter />
-        <RatingFilter />
-    </aside>
-)
+type FilterProps = {
+    selectedGenres: string[]
+    selectedRating: number | undefined
+    onGenreChange: (genre: string) => void
+    onRatingChange: (rating: number) => void
+}
+
+export const Filters = ({
+    selectedGenres,
+    selectedRating,
+    onGenreChange,
+    onRatingChange,
+}: FilterProps) => {
+    return (
+        <aside>
+            <header>
+                <h3>Filters</h3>
+                <button type="button">Clear All</button>
+            </header>
+
+            <GenreFilter
+                selectedGenres={selectedGenres}
+                onChange={onGenreChange}
+            />
+
+            <RatingFilter
+                selectedRating={selectedRating}
+                onChange={onRatingChange}
+            />
+        </aside>
+    )
+}
--- src/components/Shop/Shop.tsx
+++ src/components/Shop/Shop.tsx
@@ -1,17 +1,16 @@
 import { useState } from 'react'
-import { GenreFilter } from './GenreFilter'
-import { RatingFilter } from './RatingFilter'
 import { GameGrid } from './GameGrid'
+import { Filters } from './Filters'
 
 export const Shop = () => {
     const [selectedGenres, setSelectedGenres] = useState<string[]>([])
     const [selectedRating, setSelectedRating] = useState<number | undefined>()
 
-    const handleGenreChange = (genre: string) => {
+    const handleGenreChange = (slug: string) => {
         setSelectedGenres((prev) =>
-            prev.includes(genre)
-                ? prev.filter((id) => id !== genre)
-                : [...prev, genre]
+            prev.includes(slug)
+                ? prev.filter((selectedSlug) => selectedSlug !== slug)
+                : [...prev, slug]
         )
     }
 
@@ -21,14 +20,11 @@ export const Shop = () => {
 
     return (
         <>
-            <GenreFilter
+            <Filters
                 selectedGenres={selectedGenres}
-                onChange={handleGenreChange}
-            />
-
-            <RatingFilter
                 selectedRating={selectedRating}
-                onChange={handleRatingChange}
+                onGenreChange={handleGenreChange}
+                onRatingChange={handleRatingChange}
             />
 
             <GameGrid
