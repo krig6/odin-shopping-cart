5e13a3a1b864dd505f2c7d1578f9d9cd943b2c66
krig6 <ej.ldsm@gmail.com>
2026-09-24 11:20:47 +0800

    refactor(Navbar): render navbar links from array
    - Extract navigation links into a reusable constant
    - Render links using map

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,19 @@
 import { Link } from 'react-router'
 
+const LINKS = [
+    { to: '/', label: 'Home' },
+    { to: '/shop', label: 'Shop' },
+    { to: '/cart', label: 'Cart' },
+]
+
 export const Navbar = () => (
     <nav className="flex-1">
         <ul className="flex justify-center gap-10">
-            <li>
-                <Link to="/">Home</Link>
-            </li>
-            <li>
-                <Link to="/shop">Shop</Link>
-            </li>
-            <li>
-                <Link to="/cart">Cart</Link>
-            </li>
+            {LINKS.map((link) => (
+                <li>
+                    <Link to={link.to}>{link.label}</Link>
+                </li>
+            ))}
         </ul>
     </nav>
 )
