b76c5031ca13e14b96f7f06f905b90987524707f
krig6 <ej.ldsm@gmail.com>
2026-09-24 12:01:45 +0800

    style(Navbar): add navbar hover underline
    - Add animated underline effect to navigation links
    - Add missing key prop to mapped navigation items

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -10,8 +10,13 @@ export const Navbar = () => (
     <nav className="flex-1">
         <ul className="flex justify-center gap-10">
             {LINKS.map((link) => (
-                <li>
-                    <Link to={link.to}>{link.label}</Link>
+                <li key={link.label}>
+                    <Link
+                        to={link.to}
+                        className="relative text-[#F2E4D1] after:absolute after:-bottom-1 after:left-1/2 after:h-0.5 after:w-0 after:-translate-x-1/2 after:bg-[#E5C158] after:transition-all after:duration-300 hover:after:w-full"
+                    >
+                        {link.label}
+                    </Link>
                 </li>
             ))}
         </ul>
